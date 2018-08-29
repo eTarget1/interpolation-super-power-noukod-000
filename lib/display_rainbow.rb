a7f5f3753d3e86c2b0be4f36a6585c890828fd92
clifford <clifford@noukod.com>
2018-08-29 14:52:49 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(color)
-  puts puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts puts "Name: #{color[0]}, color: #{color[1]}, Occupation: #{color[2]}"
+  color     = ["R", red, "O", "orange", "Y", yellow, "G", green, "B", blue, "I", indigo, "V", violet]
 end
